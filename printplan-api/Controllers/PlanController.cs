e518069b88adac1e3a364bafdc6bf25cea455dce
titouanfuchs <developper@titouanfuchs.fr>
2023-09-13 16:10:08 +0200

    chore: added Delete methode to Plan Route

--- printplan-api/Controllers/PlanController.cs
+++ printplan-api/Controllers/PlanController.cs
@@ -15,20 +15,42 @@ public class PlanController : ControllerBase
     /// <returns>Estimation d'impression à la chaine</returns>
     /// <response code="201">Retourne l'estimation d'impression à la chaine</response>
     /// <response code="400">Les données d'entrée sont incorrectes</response>
-    /// <remarks>
-    /// Sample Request :
-    /// 
-    ///     POST /Plan
-    ///     {
-    ///         "quantity": 12,
-    ///         "printModelId": 5 
-    ///     }
-    /// 
-    /// </remarks>
     [HttpPost()]
     [ProducesResponseType(typeof(PrintPlanDto),StatusCodes.Status201Created)]
     [ProducesResponseType(typeof(BaseResponse), StatusCodes.Status400BadRequest)]
     public async Task<ActionResult<PrintPlanDto>> Plan(PostPrintPlanDto input)
+    {
+        return BadRequest(new BaseResponse() {Message = "Not Implemented"});
+    }
+
+    /// <summary>
+    /// Modifier les paramètres d'une planification
+    /// </summary>
+    /// <param name="id">Identifiant de la planification à modifier</param>
+    /// <param name="input">DTO planification</param>
+    /// <returns>Estimation d'impression à la chaine</returns>
+    /// <response code="201">Retourne l'estimation d'impression à la chaine</response>
+    /// <response code="400">Les données d'entrée sont incorrectes</response>
+    /// <response code="404">La planification avec l'id fournis n'éxiste pas</response>
+    [HttpPatch("{id}")]
+    [ProducesResponseType(typeof(PrintPlanDto),StatusCodes.Status202Accepted)]
+    [ProducesResponseType(typeof(BaseResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(BaseResponse), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<PrintPlanDto>> ModifyPlan(PostPrintPlanDto input, int id)
+    {
+        return BadRequest(new BaseResponse() {Message = "Not Implemented"});
+    }
+    
+    /// <summary>
+    /// Supprimer / Annuler une planification existante
+    /// </summary>
+    /// <param name="id">Identifiant de la planification à modifier</param>
+    /// <response code="200">Retourne l'estimation d'impression à la chaine</response>
+    /// <response code="404">La planification avec l'id fournis n'éxiste pas</response>
+    [HttpDelete("{id}")]
+    [ProducesResponseType(typeof(BaseResponse),StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(BaseResponse), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<BaseResponse>> DeletePlan(int id)
     {
         return BadRequest(new BaseResponse() {Message = "Not Implemented"});
     }
