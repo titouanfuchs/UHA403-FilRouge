6905082e1d0136d951795644191413f6a72ade5f
titouanfuchs <developper@titouanfuchs.fr>
2023-09-13 16:35:02 +0200

    chore: added printer controller and route

---
+++ printplan-api/Controllers/PrinterController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using printplan_api.Models.DTO;
+using printplan_api.Models.DTO.Printer;
+
+namespace printplan_api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Produces("application/json")]
+public class PrinterController : Controller
+{
+    /// <summary>
+    /// Modifier les paramètres d'une imprimante 3D
+    /// </summary>
+    /// <param name="id">Identifiant de la planification à modifier</param>
+    /// <response code="200">Le ou les paramètres de l'imprimante ont été modifiés</response>
+    /// <response code="400">Les données d'entrée sont incorrectes</response>
+    /// <response code="404">L'imprimante avec l'id fournis n'éxiste pas</response>
+    [HttpPatch("{id}")]
+    [ProducesResponseType(typeof(PrinterSettingsDTO),StatusCodes.Status202Accepted)]
+    [ProducesResponseType(typeof(BaseResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(BaseResponse), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> ChangePrinterSettings(int id)
+    {
+        return BadRequest(new BaseResponse() {Message = "Not Implemented"});
+    }
+}
---
+++ printplan-api/Models/DTO/Printer/PrinterSettingsDTO.cs
@@ -0,0 +1,19 @@
+namespace printplan_api.Models.DTO.Printer;
+
+public struct PrinterSettingsDTO
+{
+    /// <summary>
+    /// Nom de l'imprimante 3D
+    /// </summary>
+    public string Name { get; set; }
+    
+    /// <summary>
+    /// Vitesse d'impression de l'imprimante 3D en mm/s
+    /// </summary>
+    public float PrinterSpeed { get; set; }
+    
+    /// <summary>
+    /// Durée de préchauffage en secondes
+    /// </summary>
+    public float PreaheatingDuration { get; set; }
+}
