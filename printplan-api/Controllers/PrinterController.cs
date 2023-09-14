17944b92f4a0b6d7fb8fc5d0aeb02d8fd578eaca
titouanfuchs <developper@titouanfuchs.fr>
2023-09-14 12:35:13 +0200

    chore: added PrinterSettings Dto to request body

--- printplan-api/Controllers/PrinterController.cs
+++ printplan-api/Controllers/PrinterController.cs
@@ -13,6 +13,7 @@ public class PrinterController : Controller
     /// Modifier les paramètres d'une imprimante 3D
     /// </summary>
     /// <param name="id">Identifiant de la planification à modifier</param>
+    /// <param name="input">Paramètres de l'imprimante</param>
     /// <response code="200">Le ou les paramètres de l'imprimante ont été modifiés</response>
     /// <response code="400">Les données d'entrée sont incorrectes</response>
     /// <response code="404">L'imprimante avec l'id fournis n'éxiste pas</response>
@@ -20,7 +21,7 @@ public class PrinterController : Controller
     [ProducesResponseType(typeof(PrinterSettingsDTO),StatusCodes.Status202Accepted)]
     [ProducesResponseType(typeof(BaseResponse), StatusCodes.Status404NotFound)]
     [ProducesResponseType(typeof(BaseResponse), StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> ChangePrinterSettings(int id)
+    public async Task<IActionResult> ChangePrinterSettings(int id, PrinterSettingsDTO input)
     {
         return BadRequest(new BaseResponse() {Message = "Not Implemented"});
     }
