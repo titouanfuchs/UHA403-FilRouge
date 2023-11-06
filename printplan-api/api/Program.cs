88bcea2df107309cfa802e40f2ad0d3d0bb194f6
titouanfuchs <developper@titouanfuchs.fr>
2023-11-06 13:43:53 +0100

    chore: added PlanService to DependencyInjection

--- printplan-api/api/Program.cs
+++ printplan-api/api/Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddTransient<PrinterService>();
+builder.Services.AddTransient<PlanService>();
 
 builder.Services.AddDbContext<PrintPlanContext>(options =>
 {
--- printplan-api/api/Services/PlanService.cs
+++ printplan-api/api/Services/PlanService.cs
@@ -1,6 +1,20 @@
+using printplan_api.Contexts;
+using printplan_api.Models.DTO;
+
 namespace printplan_api.Services;
 
 public class PlanService
 {
-    
+    private readonly PrintPlanContext _context;
+    public PlanService(PrintPlanContext context)
+    {
+        _context = context;
+    }
+
+    public PrintPlanDto Plan(PrintPlanDto input)
+    {
+        
+        
+        return new();
+    }
 }
