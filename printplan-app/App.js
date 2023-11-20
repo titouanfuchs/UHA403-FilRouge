7126cc1a7ae38bbc914b7b7229ebe61e28fda572
titouanfuchs <developper@titouanfuchs.fr>
2023-11-20 14:26:06 +0100

    feat: added Model card

--- printplan-app/App.js
+++ printplan-app/App.js
@@ -4,37 +4,38 @@ import {NavigationContainer} from "@react-navigation/native";
 import {SafeAreaProvider} from "react-native-safe-area-context";
 import React from "react";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import {StatusBar} from "expo-status-bar";
 
 const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
   return (
       <SafeAreaProvider>
-        <NavigationContainer>
-          <Tab.Navigator initialRouteName={"Models"}>
-            <Tab.Screen name={"Models"} component={ModelsView} options={{
-              tabBarLabel: 'Models',
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons name="video-3d" color={color} size={26} />
-              )}}
-            />
-            <Tab.Screen name="Bobines" component={ModelsView} options={{
-                tabBarLabel: 'Bobines',
-                tabBarIcon: ({ color }) => (
-                    <MaterialCommunityIcons name="lambda" color={color} size={26} />
-                )}}/>
-            <Tab.Screen name="Imprimantes" component={ModelsView} options={{
-                tabBarLabel: 'Imprimantes',
-                tabBarIcon: ({ color }) => (
-                    <MaterialCommunityIcons name="printer-3d" color={color} size={26} />
-                )}}/>
-            <Tab.Screen name="Planifications" component={ModelsView} options={{
-                tabBarLabel: 'Planifications',
-                tabBarIcon: ({ color }) => (
-                    <MaterialCommunityIcons name="floor-plan" color={color} size={26} />
-                )}}/>
-          </Tab.Navigator>
-        </NavigationContainer>
+          <NavigationContainer>
+              <Tab.Navigator initialRouteName={"Models"}>
+                  <Tab.Screen name={"Models"} component={ModelsView} options={{
+                      tabBarLabel: 'Models',
+                      tabBarIcon: ({ color }) => (
+                          <MaterialCommunityIcons name="video-3d" color={color} size={26} />
+                      )}}
+                  />
+                  <Tab.Screen name="Bobines" component={ModelsView} options={{
+                      tabBarLabel: 'Bobines',
+                      tabBarIcon: ({ color }) => (
+                          <MaterialCommunityIcons name="lambda" color={color} size={26} />
+                      )}}/>
+                  <Tab.Screen name="Imprimantes" component={ModelsView} options={{
+                      tabBarLabel: 'Imprimantes',
+                      tabBarIcon: ({ color }) => (
+                          <MaterialCommunityIcons name="printer-3d" color={color} size={26} />
+                      )}}/>
+                  <Tab.Screen name="Planifications" component={ModelsView} options={{
+                      tabBarLabel: 'Planifications',
+                      tabBarIcon: ({ color }) => (
+                          <MaterialCommunityIcons name="floor-plan" color={color} size={26} />
+                      )}}/>
+              </Tab.Navigator>
+          </NavigationContainer>
       </SafeAreaProvider>
   );
 }
--- printplan-app/Views/Models/ModelsView.js
+++ printplan-app/Views/Models/ModelsView.js
@@ -1,7 +1,22 @@
-import {View} from "react-native";
+import {FlatList, SafeAreaView, SectionList, View} from "react-native";
+import ModelComponent from "../../Components/Models/ModelComponent";
+import {StatusBar} from "expo-status-bar";
+import SafeViewAndroid from "../../Components/SafeView/SafeViewAndroid";
+
+const models = [
+    {
+        id: 1,
+        name: "Test Model",
+        requiredFilamentLenght: 100.0
+    }
+]
 
 export default function ModelsView(){
-    return <View>
-        
-    </View>
+    return <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
+        <FlatList
+            data={models}
+            renderItem={({item}) => <ModelComponent model={item} />}
+            keyExtractor={item => item.id}
+        />
+    </SafeAreaView>
 }
