af8efd351e5bc05d0096a09322e5462127720e88
titouanfuchs <developper@titouanfuchs.fr>
2023-11-20 11:00:21 +0100

    feat: added navbar

--- printplan-app/App.js
+++ printplan-app/App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {createMaterialBottomTabNavigator} from "react-native-paper/react-navigation";
+import ModelsView from "./Views/Models/ModelsView";
+import {NavigationContainer} from "@react-navigation/native";
+import {SafeAreaProvider} from "react-native-safe-area-context";
+import React from "react";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Tab.Navigator initialRouteName={"Models"}>
+            <Tab.Screen name={"Models"} component={ModelsView} options={{
+              tabBarLabel: 'Models',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="video-3d" color={color} size={26} />
+              )}}
+            />
+            <Tab.Screen name="Bobines" component={ModelsView} options={{
+                tabBarLabel: 'Bobines',
+                tabBarIcon: ({ color }) => (
+                    <MaterialCommunityIcons name="lambda" color={color} size={26} />
+                )}}/>
+            <Tab.Screen name="Imprimantes" component={ModelsView} options={{
+                tabBarLabel: 'Imprimantes',
+                tabBarIcon: ({ color }) => (
+                    <MaterialCommunityIcons name="printer-3d" color={color} size={26} />
+                )}}/>
+            <Tab.Screen name="Planifications" component={ModelsView} options={{
+                tabBarLabel: 'Planifications',
+                tabBarIcon: ({ color }) => (
+                    <MaterialCommunityIcons name="floor-plan" color={color} size={26} />
+                )}}/>
+          </Tab.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ printplan-app/Views/Models/ModelsView.js
@@ -0,0 +1,7 @@
+import {View} from "react-native";
+
+export default function ModelsView(){
+    return <View>
+        
+    </View>
+}
